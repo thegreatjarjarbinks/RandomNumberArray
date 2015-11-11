4ccfe8a47b95823782e33891cdd528202cc9ec7c
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-11-11 13:02:07 -0500

    The great mighty array

---
+++ RandomNumberArray/RandomNumberArray.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RandomNumberArray
+{
+    partial class frmRandomNumberArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstArray = new System.Windows.Forms.ListBox();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstArray
+            // 
+            this.lstArray.FormattingEnabled = true;
+            this.lstArray.Location = new System.Drawing.Point(75, 12);
+            this.lstArray.Name = "lstArray";
+            this.lstArray.Size = new System.Drawing.Size(120, 95);
+            this.lstArray.TabIndex = 0;
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAverage.Location = new System.Drawing.Point(96, 204);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 27);
+            this.btnAverage.TabIndex = 1;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(13, 136);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(81, 13);
+            this.lblAverage.TabIndex = 2;
+            this.lblAverage.Text = "The average is:";
+            // 
+            // frmRandomNumberArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.lstArray);
+            this.Name = "frmRandomNumberArray";
+            this.Text = "Random Number Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstArray;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
+
---
+++ RandomNumberArray/RandomNumberArray.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by: Alex Mathias
+ * Created on: 11-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 5-01
+ * This program displays an array between 1 and 10
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumberArray
+{
+    public partial class frmRandomNumberArray : Form
+    {
+        // global variables
+        int[] arrayOfNumbers = new int[10];
+        public frmRandomNumberArray()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            //Variables
+            int loopCounter = 0;
+            int average = 0;
+            int randomNumbers;
+            Random rnd = new Random();
+
+            while (loopCounter < 10)
+            {
+                randomNumbers = rnd.Next(1, 11);
+                arrayOfNumbers[loopCounter] = randomNumbers;
+                this.lstArray.Items.Add(arrayOfNumbers[loopCounter]);
+                average = average + arrayOfNumbers[loopCounter];
+                loopCounter++;
+            }
+
+            average = average / 10;
+            this.lblAverage.Text = "The average is " + average;
+
+        }
+    }
+}
